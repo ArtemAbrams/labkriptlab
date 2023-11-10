d1b336bfabfdc20f372eb7194577387dced9be0b
artem <barishartem@gmail.com>
2023-11-11 01:27:04 +0200

    add project

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        BigStepSmallStep.main();
+        ChineseRemainderTheorem.main();
+        Cipolla.main();
+        MillerRabin.main();
+        NumberFunctions.main();
+        PollardsRho.main();
+        RSA.main();
+        SymbolCalculator.main();
+        EllipticCurve.main();
     }
 }
